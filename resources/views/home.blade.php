698f56c9723ae231a7a1e0679914b11f28d78638
Esam Khalid <esam-khalid@outlook.com>
2024-11-20 12:52:41 +0000

    added categories to the homepage which will route to the products page and only show those products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,36 @@
     </head>
     <body>
         @include('layouts.navbar')
-        <p class="text-6xl">Welcome To HiveMind</p>
+        <!-- <div class="flex justify-between [&>*]:h-[50px] [&>*]:bg-amber [&>*]:w-screen [&>*]:mx-[2px] [&>*]:rounded [&>*]:text-center [&>*]:justify-center mt-1 mx-1">
+            <div><p>Skincare</p></div>
+            <div><p>Health</p></div>
+            <div><p>Beauty</p></div>
+            <div><p>Haircare</p></div>
+            <div><p>Merch</p></div>
+        </div> -->
+        <div class="categories w-screen">
+            <ul class="list-none flex justify-between mt-1 mx-1">
+                <li>
+                    <p>SkinCare</p>
+                </li>
+                <li>
+                    <p>Health</p>
+                </li>
+                <li>
+                    <p>Beauty</p>
+                </li>
+                <li>
+                    <p>Haircare</p>
+                </li>
+                <li>
+                    <p>Merch</p>
+                </li>
+            </ul>
+
+            
+        </div>
+  
+        
+        <p class="m-[10px] text-6xl">Welcome To HiveMind</p>
     </body>
 </html>
