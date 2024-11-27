4aa56d151e1e1fc0dcacd417708d37a0adb17bd9
Taran C <t.chana.21.101@gmail.com>
2024-11-27 21:31:13 +0000

    Added & updated about us page and added pictures for this page. v1

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <title>About Us</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         @include('layouts.navbar')
-        <p class="text-6xl">About Us</p>
+        <br>
+        <div class="aboutUsPage">
+            <h2 class="text-5xl font-semibold tracking-tight text-black sm:text-7xl text-center margin-top: 1rem">About Hivemind</h2>
+            <br>
+            <p class="text-2xl text-center font-style: italic">
+                By purchasing our products, you are supporting our mission to save the bees.
+            </p>
+            <br>
+            <div class="flex justify-center">
+                <img src="/Images/beekeeperfamily.jpeg" alt="beekeeper and son"></img>
+            </div>
+            <br>
+            <div class="text-center">
+                <p class="list-inside text-2xl list-disc font-style: italic">
+                    Hivemind is a community driven by a committment to bees and the environment.<br>
+                    We are dedicated to raising awareness about the conservation of bees and habitat preservation.<br>
+                    Our products are made with natural ingredients and are cruelty free.<br>
+                    <!--<img src="/Images/beedaisy.jpeg" alt="bee on a daisy"></img>-->
+                </ul>
+            </div>
+        </div>
     </body>
 </html>
