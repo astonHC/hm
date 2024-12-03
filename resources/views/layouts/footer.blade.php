77ddf4b4e46c0a6fb361be35df83b4f6ad00c80b
Taran C <t.chana.21.101@gmail.com>
2024-12-03 18:17:35 +0000

    footer fully developed for mvp, and is usable across other pages. pictures of honeycombs for background of footer!

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<footer>
-    <div class="container mx-auto flex flex-wrap justify-evenly items-center">
+<footer style="background-image:url(/Images/honeycomb2.png);">
+    <div class="container mx-auto flex flex-wrap justify-evenly items-start">
         <!-- adding first third of footer (left side), includes contact information / link to contact page -->
         <div class="w-full md:w-1/3">
             <h3 class="text-2xl font-semibold">Contact Us</h3>
@@ -11,13 +11,14 @@
             <a href="{{ route('contact') }}" class="text-lg text-blue-500 hover:text-blue-600">Contact Page</a>
         </div>
         <!-- adding second third of footer (middle), includes donation button -->
-        <div class="w-full md:w-1/3 mt-4 md:mt-0">
-        <h3 class="text-2xl font-semibold">Support Our Cause</h3>
-        <p class="text-lg">Help us save the bees by donating today!</p>
-        <a href="https://www.paypal.com/donate/?hosted_button_id=VZZ8YQ994DXUE" class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded mt-4 inline-block">Donate</a>
+        <div class="w-full md:w-1/3 mt-4 md:mt-0 flex flex-col items-start">
+            <h3 class="text-2xl font-semibold">Support Our Cause</h3>
+            <p class="text-lg">Help us save the bees by donating today!</p>
+            <a href="https://www.paypal.com/donate/?hosted_button_id=VZZ8YQ994DXUE" class="bg-white hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mt-4 inline-block">Donate</a>
     </div>
     <div class="w-full md:w-1/3 mt-4 md:mt-0">
         <!-- adding third third of footer (right side), includes google maps embed-->
+        <h3 class="text-2xl font-semibold">Find Us Here!</h3>
         <div class="relative h-64 w-full">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629667.5904227577!2d-7.296735727730728!3d51.9405375026017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x484481318da1a1bb%3A0xf6348014bd14200!2sHive%20Mind!5e0!3m2!1sen!2suk!4v1733242255053!5m2!1sen!2suk" width="100%" height="100%" style="border:0" allowfullscreen="" loading="lazy"></iframe>
         </div>
