a80c24fc3eb983fe76db249e8b92bc7fe39bcaa1
Aryanjot Grewal <aryanzgrewal@gmail.com>
2024-12-04 13:37:55 +0000

    Update orders.blade.php

--- resources/views/orders/orders.blade.php
+++ resources/views/orders/orders.blade.php
@@ -54,7 +54,7 @@
                 @foreach ($orders as $order)
                     <div class="bg-white shadow-md rounded-lg p-6 transition-transform hover:scale-105 hover:shadow-lg">
                         <div class="mb-4">
-                            <h3 class="text-2xl font-bold text-blue-600">Product: {{ $order['product_name'] }}</h3>
+                            <h3 class="text-2xl font-bold text-grey-800">Product: {{ $order['product_name'] }}</h3>
                         </div>
                         <div class="text-gray-700">
                             <p class="mb-2"><strong>Order ID:</strong> {{ $order['order_id'] }}</p>
