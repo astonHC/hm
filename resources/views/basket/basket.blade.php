2257ef0ca03f4f09a45eaf8f4593ed1e0bf65e00
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2024-12-04 10:29:41 +0000

    basket 👍

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -28,15 +28,15 @@
         for (let i of listItems) {
             if (!document.querySelector(`#quantity-input-${i.id}`)) {
                 var item = document.createElement('li');
-                item.className = 'bg-amber rounded-lg mb-10 p-10 flex justify-between items-center xs:p-0';
+                item.className = 'bg-yellow-200 rounded-lg mb-10 p-10 flex justify-between items-center xs:p-0';
                 item.id=`${i.id}`
                 item.innerHTML =
                     `<span class="item-img-text-link flex text-wrap"><img class="w-[100px] h-[100px]"
                             src=${i.img} width="130px"
                             height="130px" alt="IMAGE" />
-                        <div class="ml-3 max-w-[50%]"><a class="text-2xl text-nowrap">${i.name}</a>
-                            <p id="item-description" class="text-base leading-5 w-[100%]">${i.description}</p>
-                            <p id="item-price" class="text-base font-bold leading-9">£${i.price}</p>
+                        <div class="ml-3 max-w-[50%]"><a class="text-3xl text-nowrap">${i.name}</a>
+                            <p id="item-description" class="text-lg leading-5 w-[100%]">${i.description}</p>
+                            <p id="item-price" class="text-lg font-bold leading-9">£${i.price}</p>
                         </div>
                     </span>
                     <!-- FORM FOR QTY BUTTONS -->
@@ -46,29 +46,29 @@
 
                             <button type="button" onClick="loseOne(${i.id})" id="decrement-button"
                                 data-input-counter-decrement="${i.id}"
-                                class="w-[40px] bg-yellow-100 dark:bg-yellow-700 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border border-yellow-300 rounded-s-lg p-3 h-11 focus:ring-yellow-100 dark:focus:ring-yellow-700 focus:ring-2 focus:outline-none">
+                                class="w-[40px] bg-yellow-100 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border border-yellow-300 rounded-s-lg p-3 h-11 focus:ring-yellow-100 dark:focus:ring-yellow-500 focus:ring-2 focus:outline-none">
 
                                 <!-- minus icon -->
-                                <svg class="w-3 h-3 text-yellow-900 dark:text-white" aria-hidden="true"
+                                <svg class="w-3 h-3 text-white dark:text-grey-300" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="M1 1h16" />
                                 </svg>
-
                             </button>
+                            
                             <input type="text" id="quantity-input" data-input-counter
                                 data-input-counter-min="1" data-input-counter-max="50"
                                 aria-describedby="helper-text-explanation"
-                                class="quantity-input w-[70px] bg-yellow-50 border-x-0 border-yellow-300 h-11 text-center text-yellow-900 text-sm focus:ring-blue-500 focus:border-blue-500 block py-2.5 dark:bg-yellow-700 dark:border-yellow-600 dark:placeholder-yellow-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="quantity-input w-[70px] bg-yellow-50 border-x-0 border-yellow-300 h-11 text-center text-white text-sm focus:ring-blue-500 focus:border-blue-500 block py-2.5 dark:bg-yellow-500 dark:border-yellow-600 dark:placeholder-yellow-400 dark:text-grey-300 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="${i.quantity}" value="1" required />
 
                             <!-- DATABASE REQUIRED FOR VALUE -->
                             <button type="button" onClick="addOne(${i.id})" id="increment-button"
                                 data-input-counter-increment="${i.id}"
-                                class="w-[40px] bg-yellow-100 dark:bg-yellow-700 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border border-yellow-300 rounded-e-lg p-3 h-11 focus:ring-yellow-100 dark:focus:ring-yellow-700 focus:ring-2 focus:outline-none">
+                                class="w-[40px] bg-yellow-100 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border border-yellow-300 rounded-e-lg p-3 h-11 focus:ring-yellow-100 dark:focus:ring-yellow-500 focus:ring-2 focus:outline-none">
 
                                 <!--plus icon -->
-                                <svg class="w-3 h-3 text-yellow-900 dark:text-white" aria-hidden="true"
+                                <svg class="w-3 h-3 text-white dark:text-grey-300" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="M9 1v16M1 9h16" />
@@ -78,7 +78,7 @@
                         
                     </form>
                     <input type="checkbox" checked="true" onclick="getTotal()"
-                        class="isinOrder bg-yellow-100 dark:bg-yellow-700 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border checked:bg-yellow-700 focus:bg-yellow-700 active:bg-yellow-700 ml-8 mt-auto mb-auto" />
+                        class="isinOrder bg-yellow-100 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:border-yellow-600 hover:bg-yellow-200 border checked:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 ml-8 mt-auto mb-auto" />
                     </div>
                     `
                 document.getElementById("items").append(item);
@@ -155,21 +155,21 @@
 
     <div class="flex flex-wrap lg:flex-nowrap" onload="populateBasket()">
         <div id="basket-items-wrapper"
-            class="min-w-[700px] w-[100%] bg-yellow-300 text-3xl text-white rounded-lg p-10 m-10 mt-10 lg:w-[80%]">
+            class="min-w-[700px] w-[100%] bg-yellow-50 text-3xl text-grey-300 rounded-lg p-10 m-10 mt-10 lg:w-[80%]">
             <p class="text-6xl mt-10 mb-10">Basket</p>
             <ul id="items" onload="populateBasket()">
                  
             </ul>
         </div>
         <div
-            class=" bg-yellow-300 text-3xl text-white rounded-lg  ml-5 mt-10 mr-5 mb-10 flex-wrap min-w[160px] w-[100%] lg:w-max md:flex-nowrap md:p-10 lg:relative ">
+            class=" bg-yellow-50 text-3xl text-grey-300 rounded-lg  ml-5 mt-10 mr-5 mb-10 flex-wrap min-w[160px] w-[100%] lg:w-max md:flex-nowrap md:p-10 lg:relative ">
             <div class="ml-5 mt-10">
                 <p>Items in basket:<span id="count"> </span></p>
                 <p>Total:</p>
                 <span id="total"></span>
             </div>
 
-            <button class="bg-amber rounded-lg mb-10 p-4 mr-5 ml-5 mt-10 w-full h-56 lg:h-24 text-7xl lg:text-3xl"
+            <button class="bg-yellow-400 hover:bg-yellow-500 rounded-lg mb-10 p-4 mr-5 ml-5 mt-10 w-full h-56 lg:h-24 text-7xl lg:text-3xl"
                 onClick="addCheckedItemsToCheckout()"> GO
                 TO
                 CHECKOUT</button>
