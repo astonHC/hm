4eca77cc0ee7ae250cd410b0ec8cacd2ad92fe92
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-05 19:14:05 +0000

    Changed a bit of the layout to match market research

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>User {{ $product->id }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <title>{{ $product->product_name }}</title>
     </head>
     <body>
-        <h1>User first name {{ $product->product_name }}</h1>
-        <h1>User last name {{ $product->product_type }}</h1>
+        @include('layouts.navbar')
+        <a
+            href="{{ route('products') }}"
+            class="fas fa-arrow-left fa-4x p-5"
+        ></a>
+        <div class="flex justify-center">
+            <div class="flex justify-center">
+                <img class="" src="{{ asset('../Images/placeholder.avif') }}" />
+            </div>
+            <div class="inline-block justify-center">
+                <h1 class="text-5xl pb-4">{{ $product->product_name }}</h1>
+                <h1 class="text-5xl pb-4">{{ $product->price }}</h1>
+            </div>
+        </div>
     </body>
 </html>
