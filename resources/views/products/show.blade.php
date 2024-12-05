827512b2dd42536c58c0cd669738b1633e430b3e
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-05 20:18:56 +0000

    more layout changes

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -11,12 +11,32 @@
             class="fas fa-arrow-left fa-4x p-5"
         ></a>
         <div class="flex justify-center">
-            <div class="flex justify-center">
+            <div class="flex justify-center pr-5">
                 <img class="" src="{{ asset('../Images/placeholder.avif') }}" />
             </div>
-            <div class="inline-block justify-center">
-                <h1 class="text-5xl pb-4">{{ $product->product_name }}</h1>
-                <h1 class="text-5xl pb-4">{{ $product->price }}</h1>
+            <div class="inline-block justify-center size-[400px]">
+                <div class="flex justify-center">
+                    <h1 class="text-5xl pb-4">{{ $product->product_name }}</h1>
+                </div>
+                <h1 class="text-3xl pb-4">£{{ $product->price }}</h1>
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris vehicula ex a mauris commodo gravida. Aliquam a
+                    pulvinar eros. Mauris lacinia, urna eu mollis ullamcorper,
+                    ex nulla ultrices sapien, sed tincidunt lectus nisl quis
+                    dolor. Morbi scelerisque consectetur arcu, pellentesque
+                    tempor metus. Vestibulum sit amet vestibulum purus, sed
+                    cursus turpis. Etiam id sem fringilla, vestibulum mauris
+                    eget, bibendum dolor. Vestibulum iaculis risus at tristique
+                    venenatis. Pellentesque nisi nisi, porttitor ac venenatis
+                    et, lacinia in risus. Nulla justo enim, scelerisque sit amet
+                    vestibulum nec, sollicitudin at tellus. Fusce tempor lacus
+                    nec dapibus rhoncus. Vestibulum vel ex eu turpis efficitur
+                    fermentum.
+                </p>
+                <button type="button" class="border border-black">
+                    Add to Basket
+                </button>
             </div>
         </div>
     </body>
