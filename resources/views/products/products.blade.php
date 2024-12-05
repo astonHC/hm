4a9fb746e32e1190a1f0e96aaaf875fed9f5996a
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-05 22:18:47 +0000

    working on the grid gaps

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -8,13 +8,16 @@
         <p class="text-6xl">Products</p>
 
         <form action="{{ route('products') }}" method="GET">
-            <input type="text" name="product_name" value="{{ request('product_name') }}"/>
+            <input
+                type="text"
+                name="product_name"
+                value="{{ request('product_name') }}"
+            />
             <br />
             <button type="submit">Submit</button>
         </form>
 
-        
-        <form action="{{ route('products') }}" method="GET">
+        <!-- <form action="{{ route('products') }}" method="GET">
                 <select name="filter" onchange="this.form.submit()">
                 <option value="none"{{ request('filter') == 'none' ? 'selected' : '' }}>None</option>
                 <option value="Skincare" {{ request('filter') == 'Skincare' ? 'selected' : '' }}>Skincare</option>
@@ -23,16 +26,18 @@
                 <option value="Haircare" {{ request('filter') == 'Haircare' ? 'selected' : '' }}>Haircare</option>
                 <option value="Merch" {{ request('filter') == 'Merch' ? 'selected' : '' }}>Merch</option>
             </select>
-        </form>
+        </form> -->
 
         @if(count($products) === 0)
         <p>No products found</p>
         @else
 
-        <div class="grid grid-cols-4">
+        <div class="grid grid-cols-5 gap-0">
             @foreach($products as $product)
-            <a href="{{route('products.show', $product->id)}}"
-                ><div>
+            <a
+                href="{{route('products.show', $product->id)}}"
+                class="w-fit border"
+                ><div class="size-fit">
                     <h1>{{$product->product_name}}</h1>
                     <img
                         class="size-[175px]"
@@ -45,7 +50,6 @@
 
             @endforeach
         </div>
-        @endif
-        @include('layouts.footer')  
+        @endif @include('layouts.footer')
     </body>
 </html>
