8b3a885ebfba98634d7620072bd35fc34f8e0197
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-06 22:49:59 +0000

    Added back arrow to the orders page

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -8,33 +8,33 @@
         @include('layouts.navbar')
 
         <main>
-            <div class="flex justify-center">
+            <div class="flex justify-center m-4">
                 <p class="text-6xl">Your Account</p>
             </div>
 
             <div class="flex justify-center">
-                <div
-                    class="flex justify-center w-fit h-[50vh] bg-amber rounded-lg"
-                >
-                    <div class="flex justify-center w-full m-4">
-                        <div class="flex">
-                            <a
-                                href="{{ route('logout') }}"
-                                class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
-                                >Logout</a
-                            >
-                            <a
-                                href="{{ route('orders') }}"
-                                class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
-                                >Orders</a
-                            >
-                            <a
-                                href="{{ route('orders') }}"
-                                class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4 text-nowrap"
-                                >View Account Details</a
-                            >
+                <div class="inline-block">
+                    <div class="flex justify-center">
+                        <div
+                            class="flex justify-center w-fit h-[50vh] bg-amber"
+                        >
+                            <div class="flex justify-center w-full m-4">
+                                <div class="flex">
+                                    <a
+                                        href="{{ route('logout') }}"
+                                        class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
+                                        >Logout</a
+                                    >
+                                    <a
+                                        href="{{ route('orders') }}"
+                                        class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
+                                        >Orders</a
+                                    >
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div class="cdwa-right h-[13px]"></div>
                 </div>
             </div>
         </main>
