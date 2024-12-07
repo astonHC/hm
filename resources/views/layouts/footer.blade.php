aa72be95e9f06e54ae27e6ffe4e9ebc6ac80b276
Jia Li Lin <emilyshaven@outlook.com>
2024-12-07 14:03:21 +0000

    added to homepage, few changes to css and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,35 +1,36 @@
 <footer
     style="background-image: url(/Images/honeycomb2.png)"
-    class="bottom-0 w-full h-fit-content border border-black"
+    class="bottom-0 w-full h-fit-content border border-amber"
 >
     <div class="container mx-auto flex justify-between h-fit">
         <!-- adding first third of footer (left side), includes contact information / link to contact page -->
         <div class="w-full md:w-1/3">
-            <h3 class="text-2xl font-semibold">Contact Us</h3>
-            <p class="text-lg">HiveMind</p>
-            <p class="text-lg">123 Beehive Lane</p>
-            <p class="text-lg">Dublin, Ireland</p>
-            <p class="text-lg">Phone: +353-123-4567</p>
-            <p class="text-lg">Email: admin@hivemind.com</p>
+            <h3 class="text-lg font-semibold text-gray-800">Contact Us</h3>
+            <p class="text-sm text-gray-800">HiveMind</p>
+            <p class="text-sm text-gray-800">123 Beehive Lane</p>
+            <p class="text-sm text-gray-800">Dublin, Ireland</p>
+            <p class="text-sm text-gray-800">Phone: +353-123-4567</p>
+            <p class="text-sm text-gray-800">Email: admin@hivemind.com</p>
+            <p class="text-sm text-gray-800">Have an enquiry? Fill in a 
             <a
                 href="{{ route('contact') }}"
-                class="text-lg text-blue-500 hover:text-blue-600"
-                >Contact Page</a
-            >
+                class="text-sm text-blue-500 hover:text-blue-600"
+                >contact form</a
+            ></p>
         </div>
         <!-- adding second third of footer (middle), includes donation button -->
         <div class="w-full md:w-1/3 mt-4 md:mt-0 flex flex-col items-start">
-            <h3 class="text-2xl font-semibold">Support Our Cause</h3>
-            <p class="text-lg">Help us save the bees by donating today!</p>
+            <h3 class="text-lg font-semibold text-gray-800">Support Our Cause</h3>
+            <p class="text-sm text-gray-800">Help us save the bees by donating today!</p>
             <a
                 href="https://www.paypal.com/donate/?hosted_button_id=VZZ8YQ994DXUE"
-                class="bg-white text-black hover:bg-white hover:text-amber font-bold py-2 px-4 rounded mt-4 inline-block"
+                class="bg-white text-gray-800 hover:bg-white hover:text-amber font-bold py-2 px-4 rounded mt-4 inline-block"
                 >Donate</a
             >
         </div>
-        <div class="w-full md:w-1/3 mt-4 md:mt-0">
+        <div class="w-full md:w-1/3 mt-4 md:mt-0 pb-4">
             <!-- adding third third of footer (right side), includes google maps embed-->
-            <h3 class="text-2xl font-semibold">Find Us Here!</h3>
+            <h3 class="text-lg font-semibold text-gray-800">Find Us Here!</h3>
             <div class="relative w-full h-fit">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629667.5904227577!2d-7.296735727730728!3d51.9405375026017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x484481318da1a1bb%3A0xf6348014bd14200!2sHive%20Mind!5e0!3m2!1sen!2suk!4v1733242255053!5m2!1sen!2suk"
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
         <!-- navbar -->
         <nav class="navbar flex justify-between items-center bg-amber text-white px-6 py-4">
             <!-- logo section -->
-            <a href="{{ route('home') }}" class="flex items-center hover:text-gray-200 transition duration-200">
+            <a href="{{ route('home') }}" class="flex items-center hover:text-gray-200 duration-200">
                 <img src="{{ asset('../Images/HiveMind Logo.png') }}" alt="HiveMind Logo" class="h-12 w-auto mr-3"/>
                 <h1 class="text-2xl font-bold">HiveMind</h1>
             </a>
@@ -14,24 +14,24 @@
                 <!-- navigation links -->
                 <!-- links are next to icons -->
                 <ul class="hidden md:flex space-x-8 text-lg">
-                    <li><a href="{{ route('products') }}" class="hover:text-gray-200 transition">Shop Us</a></li>
-                    <li><a href="{{ route('about') }}" class="hover:text-gray-200 transition">About Us</a></li>
-                    <li><a href="{{ route('contact') }}" class="hover:text-gray-200 transition">Contact Us</a></li>
+                    <li><a href="{{ route('products') }}" class="hover:text-gray-200">Shop Us</a></li>
+                    <li><a href="{{ route('about') }}" class="hover:text-gray-200">About Us</a></li>
+                    <li><a href="{{ route('contact') }}" class="hover:text-gray-200">Contact Us</a></li>
                 </ul>
 
                 <!-- search icon // trying to make it so that search bar appears when user clicks on icon (still a wip) -->
                 <div class="flex items-center space-x-4">
                     <!-- search bar -->
-                    <input id="searchBar" type="text" placeholder="Search" class="hidden md:block w-48 bg-white text-black placeholder-gray-500 rounded-full px-4 py-2 shadow-lg focus:outline-none focus:ring-2 focus:ring-amber-500 transition"/>
+                    <input id="searchBar" type="text" placeholder="Search" class="hidden md:block w-48 bg-white text-gray-800 placeholder-gray-500 rounded-full px-4 py-2 shadow-lg focus:outline-none focus:ring-2 focus:ring-yellow-700"/>
 
                     <!-- search icon -->
-                    <button id="searchIcon" class="text-white hover:text-gray-200 focus:outline-none" onclick="toggleSearchBar()">
+                    <button id="searchIcon" class="text-white hover:text-gray-200">
                         <i class="fas fa-search fa-xl"></i>
                     </button>
                 </div>
 
-                <a href="{{ route('basket') }}" class="fas fa-shopping-basket fa-xl hover:text-gray-200 transition"></a>
-                <a href="{{ route('account') }}" class="fas fa-user fa-xl hover:text-gray-200 transition"></a>
+                <a href="{{ route('basket') }}" class="fas fa-shopping-basket fa-xl hover:text-gray-200"></a>
+                <a href="{{ route('account') }}" class="fas fa-user fa-xl hover:text-gray-200"></a>
             </div>
         </nav>
 
