9fed4ffe2cecdc036728dfb86f4b2847d764a57f
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-09 04:33:47 +0000

    displays no items in basket message when no items are found in basket

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -9,43 +9,48 @@
 
         <main>
             <div class="flex justify-center m-4">
-                <p class="text-6xl">Basket</p>
                 <div class="inline-block">
-                    @foreach($basketItems as $basketItem)
-                    <div class="bg-amber">
-                        <p>{{ $basketItem->product_name }}</p>
-                        <p>Price: {{$basketItem->price}}</p>
-                        <p>quantity: {{$basketItem->quantity}}</p>
-                        <form
-                            action="{{ route('basket.updateQuantity') }}"
-                            method="post"
-                        >
-                            @csrf
-                            <input type="text" name="quantity" required />
-                            <input
-                                type="hidden"
-                                name="product_id"
-                                value="{{ $basketItem->product_id }}"
-                            />
-                            <br />
-                            <button type="submit">Update quantity</button>
-                            <br />
-                        </form>
-                        <form
-                            action="{{ route('basket.remove') }}"
-                            method="post"
-                        >
-                            @csrf
-                            <input
-                                type="hidden"
-                                name="product_id"
-                                value="{{ $basketItem->product_id }}"
-                            />
-                            <button type="submit">remove</button>
-                        </form>
+                    <p class="text-6xl">Basket</p>
+                    @if(count($basketItems) === 0)
+                    <p>no items in basket</p>
+                    @else
+                    <div class="inline-block">
+                        @foreach($basketItems as $basketItem)
+                        <div class="bg-amber">
+                            <p>{{ $basketItem->product_name }}</p>
+                            <p>Price: {{$basketItem->price}}</p>
+                            <p>quantity: {{$basketItem->quantity}}</p>
+                            <form
+                                action="{{ route('basket.updateQuantity') }}"
+                                method="post"
+                            >
+                                @csrf
+                                <input type="text" name="quantity" required />
+                                <input
+                                    type="hidden"
+                                    name="product_id"
+                                    value="{{ $basketItem->product_id }}"
+                                />
+                                <br />
+                                <button type="submit">Update quantity</button>
+                                <br />
+                            </form>
+                            <form
+                                action="{{ route('basket.remove') }}"
+                                method="post"
+                            >
+                                @csrf
+                                <input
+                                    type="hidden"
+                                    name="product_id"
+                                    value="{{ $basketItem->product_id }}"
+                                />
+                                <button type="submit">remove</button>
+                            </form>
+                        </div>
+                        <br />
+                        @endforeach @endif
                     </div>
-                    <br />
-                    @endforeach
                 </div>
             </div>
         </main>
